512dda4f547c1637b1a655722343a861a541375b
Jakub <derivatives_four@protonmail.com>
2024-08-10 16:06:31 +0200

    add new prop to ButtonStyled
    This styled component has been enhanced with 'width' prop which sets the minimum width for the component.

--- src/components/Buttons/ButtonStyled.js
+++ src/components/Buttons/ButtonStyled.js
@@ -1,6 +1,11 @@
 import styled from 'styled-components'
 
 const ButtonStyled = styled.button`
+  min-width:
+    ${props => props.$width
+      ? '120px'
+      : 'initial'
+    };
   padding: 10px;
   border: 1px solid black;
   border-radius: 5px;
