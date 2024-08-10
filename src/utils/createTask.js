33ddee56e0a3df337e1a7360f4d75c3c29c83305
Jakub <derivatives_four@protonmail.com>
2024-08-10 12:51:01 +0200

    correct indentation in the code block

--- src/utils/createTask.js
+++ src/utils/createTask.js
@@ -2,12 +2,12 @@ import Task from "../classes/Task/Task"
 
 function createTask(task) {
   return typeof task === 'string'
-      ? (
-        new Task(task)
-      )
-      : (
-        console.log(`createTask() accepts only string as argument.`)
-      )
+    ? (
+      new Task(task)
+    )
+    : (
+      console.log(`createTask() accepts only string as argument.`)
+    )
 }
 
 export default createTask
