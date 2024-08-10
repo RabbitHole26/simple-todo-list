ebea5bac7765dd83caa4a5ea5578452297b37871
Jakub <derivatives_four@protonmail.com>
2024-08-10 14:26:15 +0200

    remove SignUp page

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import { ListProvider } from './context/ListContext'
 import HomePage from './pages/HomePage/HomePage'
 import About from './pages/About/About'
 import Layout from './components/Layout/Layout'
-import SingUp from './pages/SingUp/SingUp'
 import './App.css'
 
 function App() {
@@ -22,7 +21,6 @@ function App() {
                   <Routes>
                     <Route path="/" element={<HomePage />} />
                     <Route path="about" element={<About />} />
-                    <Route path="sign-up" element={<SingUp />} />
                   </Routes>
                 </Layout>
               </ThemeProvider>
